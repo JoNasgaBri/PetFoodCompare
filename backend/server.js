998d234812f865645d4dd50d3ad006b09b4cb433
Jonas Gabriel <jonasgabriel9803@gmail.com>
2025-06-16 01:11:28 -0300

    Ajustes para deploy no Heroku

--- backend/server.js
+++ backend/server.js
@@ -33,5 +33,6 @@ app.post('/products/compare', (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log("Backend rodando na porta 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Backend rodando na porta ${PORT}`));
 
